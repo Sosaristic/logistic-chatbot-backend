6bccf345d4445b2a474a8e8a84f517f990955658
Sunday omena <sundayomena9@gmail.com>
2025-01-26 17:53:53 +0100

    removed trailing slash from url

--- src/app.ts
+++ src/app.ts
@@ -21,8 +21,8 @@ const corsOptions: CorsOptions = {
     'http://localhost:3000',
     'http://localhost:5000',
     'http://localhost:5173',
-    'https://cozycart.netlify.app/',
-    'https://dashboard-chatbot-tau.vercel.app/',
+    'https://cozycart.netlify.app',
+    'https://dashboard-chatbot-tau.vercel.app',
     process.env.CLIENT_URL,
   ],
 };
